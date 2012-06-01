0a1180bf72a1987f4de800b358af9eed627deee0
Renan Azevedo <renan.razevedo@gmail.com>
2012-06-01 16:24:52 -0300

    organizing fields and resolving base path for gh-pages

--- README.md
+++ README.md
@@ -2,6 +2,19 @@ Careless Admin
 ==============
 THIS PROJECT IS UNDER DEVELOPMENT AND WILL BE REALEASED SOON.
 
+Watch development
+-----------------
+# jekyll for demo pages
+jekyll --base-url /careless-admin
+
+# careless_admin(.min).css
+sass --watch scss/careless_admin.scss:assets/careless_admin.css --style expanded
+sass --watch scss/careless_admin.scss:assets/careless_admin.min.css --style compressed
+
+# typography(.min).css
+sass --watch scss/typography.scss:assets/typography.css --style expanded
+sass --watch scss/typography.scss:assets/typography.min.css --style compressed
+
 License
 -------
 Careless Admin is released under the MIT license:
--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 rm -rf assets/ && mkdir assets/
 
 # careless_admin(.min).css
-sass scss/careless_admin.scss:assets/careless_admin.css --style expanded
-sass scss/careless_admin.scss:assets/careless_admin.min.css --style compressed
+sass scss/careless-admin.scss:assets/careless-admin.css --style expanded
+sass scss/careless-admin.scss:assets/careless-admin.min.css --style compressed
 
 # typography(.min).css
 sass scss/typography.scss:assets/typography.css --style expanded
@@ -16,7 +16,7 @@ cp -r entypo assets/
 cp -r javascripts/* assets/
 
 # careless_admin.zip
-cp -r assets careless_admin 
-zip -r careless_admin.zip careless_admin
-mv careless_admin.zip assets/
-rm -rf careless_admin
+cp -r assets careless-admin 
+zip -r careless-admin.zip careless-admin
+mv careless-admin.zip assets/
+rm -rf careless-admin
