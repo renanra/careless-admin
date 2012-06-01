936260a4951409140217a5890427ea07ea153696
Renan Azevedo <renan.razevedo@gmail.com>
2012-06-01 18:29:47 -0300

    docs for js

--- javascripts/careless-admin.js
+++ javascripts/careless-admin.js
@@ -1,29 +1,41 @@
 $(function() {
 
-    // Close button for messages
-    $(".message > span a").click(function() {
-        $(this).parents(".message:eq(0)").hide();
-        return false;
-    });
+    // Messages: Close button
+    (function() {
+        $(".message > span a").click(function() {
+            $(this).parents(".message:eq(0)").hide();
+            return false;
+        });
+    })();
 
-    var resizable = $(document).width() > $('.sub-wrapper').width() && $('html').attr('class').match(/linux|mac|windows/);
-
-    if (resizable) {    
+    // Aside: Floating it
+    (function() {
         var $aside = $('.aside');
-        if ($aside.length) {
+
+        // verify if is possible
+        var resizable = 
+        // is there scroll?    
+        $(document).width() > $('.sub-wrapper').width() &&
+        // is there aside?
+        $aside.length;
+
+        if (resizable) {    
+
+            // distance from the top of the page
             var top = 10;
             var topAside = $aside.position().top;
-
             $aside.css({
                 'top' : top
             });
 
             $(document).scroll(function() {
+                // aside is going to be hidden, let's keep it fixed
                 if ($(this).scrollTop() + top > topAside) {
                     $aside.css({
                         'position' : 'fixed'
                     });
                 }
+                // just static
                 else {
                     $aside.css({
                         'position' : 'static'
@@ -31,6 +43,6 @@ $(function() {
                 }
             });
         }
-    }
+    })();
 
 });
